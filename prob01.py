449e248c56b9c677d4713e4dde11cf6bbed8dfc8
acp111 <92266971+aravparikh@users.noreply.github.com>
2025-02-20 21:01:56 -0800

    Update prob01.py

--- prob01.py
+++ prob01.py
@@ -1,3 +1,3 @@
 with open('input10.txt', 'r') as file:
         lines = file.readlines()
-print(f" Your real strength comes from being the best {lines[0] you can be}"
+print(f" Your real strength comes from being the best {lines[0]} you can be")
